fbe38c311449fdc97d803c48ecaf272449eacc81
Jens Penny <el.tutter@gmail.com>
2019-03-19 23:37:01 +0100

    initial commit of a testing application that can be loaded and ran headless in a docker container

---
+++ src/main/java/FxApplication.java
@@ -0,0 +1,78 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class FxApplication extends Application {
+
+
+    /**
+     * Testing code lifted from the site https://www.mkyong.com/javafx/javafx-hello-world-example/
+     *
+     * @param stage
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Hello World!");
+
+        GridPane grid = new GridPane();
+        grid.setHgap(0);
+        grid.setVgap(0);
+
+        Rectangle r1 = new Rectangle();
+        r1.setFill(Color.DARKKHAKI);
+        r1.setHeight(125);
+        r1.setWidth(125);
+
+        Rectangle r2 = new Rectangle();
+        r2.setFill(Color.rgb(189, 40, 40));
+        r2.setHeight(125);
+        r2.setWidth(125);
+
+        Rectangle r3 = new Rectangle();
+        r3.setFill(Color.hsb(235, 0.52, 0.36));
+        r3.setHeight(125);
+        r3.setWidth(125);
+
+        Rectangle r4 = new Rectangle();
+        r4.setFill(Color.web("b894cc"));
+        r4.setHeight(125);
+        r4.setWidth(125);
+
+        grid.add(r1, 0, 0);
+        grid.add(r2, 0, 1);
+        grid.add(r3, 1, 0);
+        grid.add(r4, 1, 1);
+
+        Label l = new Label();
+        l.setFont(new Font("Calibri", 15));
+        l.setTextFill(Color.BLACK);
+
+        Button button = new Button();
+        button.setFont(new Font("Calibri", 15));
+        button.setText("Say 'Hello World'");
+
+        button.setOnAction(event -> l.setText("You said Hello to the world"));
+
+        BorderPane bp = new BorderPane();
+        bp.setBottom(l);
+        BorderPane.setAlignment(l, Pos.CENTER);
+        bp.setCenter(button);
+
+        StackPane root = new StackPane();
+        root.getChildren().add(grid);
+        root.getChildren().add(bp);
+        stage.setScene(new Scene(root, 250, 250));
+        stage.show();
+    }
+
+}
---
+++ src/main/java/ScreenCapture.java
@@ -0,0 +1,7 @@
+public class ScreenCapture {
+
+    public static void main(String[] args) {
+
+    }
+
+}
